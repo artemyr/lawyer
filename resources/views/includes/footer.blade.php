fe85527167416bb4e9568d9ef7837c6ef41be17f
artemyr <artem.affetta@gmail.com>
2024-01-10 23:46:33 +0300

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -9,8 +9,90 @@
                 </a>
             </div>
             <div>
-                <div class="">телефон</div>
-                <div class="">кнопки</div>
+
+                <div class="footer__phone">
+                    <div class="footer__phone-number">8 (800) 232-05-05</div>
+                    <div class="footer__phone-label">Юрист перезвонит в течении 15 минут</div>
+                </div>
+
+                <div class="footer__btns">
+                    <a class="ui-btn ui-btn-arrow">
+                        <span>Заказать звонок</span>
+                        <svg>
+                            <use xlink:href="http://lawyer.localhost/svg/sprite.svg#button-plus"></use>
+                        </svg>
+                    </a>
+                    <div class="footer__to-top-btn">
+                        <svg>
+                            <use xlink:href="{{ asset('svg/sprite.svg#arrow-4') }}"></use>
+                        </svg>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <div class="footer__menu">
+                <div class="footer-menu">
+                    <ul>
+                        <li><a href="" class="footer-menu__title">Государственные инстанции</a></li>
+                        <li><a href="">Судебные приставы</a></li>
+                        <li><a href="">Налоговая</a></li>
+                        <li><a href="">Прокуратура</a></li>
+                        <li><a href="">Полиция</a></li>
+                        <li><a href="">ГИБДД</a></li>
+                        <li><a href="">Суды</a></li>
+                    </ul>
+                </div>
+                <div class="footer-menu">
+                    <div>
+                        <ul>
+                            <li><a href="" class="footer-menu__title">Контакты</a></li>
+                            <li><a href="">8 (800) 232-05-05</a></li>
+                            <li><a href="">info@lawyers-kst.ru</a></li>
+                            <li><a href="">г. Санкт-Петербург, 105066, ул.<br>Ольховская, д. 4, корп. 2г</a></li>
+                            <li><a href="">Режим работы: Пн - Пт с 9:00 до<br>18:00</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="" class="footer-menu__title">О компании</a></li>
+                            <li><a href="">Услуги</a></li>
+                            <li><a href="">Блог</a></li>
+                            <li><a href="">Контакты</a></li>
+                        </ul>
+                        <div class="footer-socials">
+                            <p class="footer-menu__title">Соц. сети</p>
+                            <div class="footer-socials__list">
+                                <a href="">
+                                    <svg>
+                                        <use xlink:href="{{ asset('svg/sprite.svg#telegram') }}"></use>
+                                    </svg>
+                                </a>
+                                <a href="">
+                                    <svg>
+                                        <use xlink:href="{{ asset('svg/sprite.svg#vk') }}"></use>
+                                    </svg>
+                                </a>
+                                <a href="">
+                                    <svg>
+                                        <use xlink:href="{{ asset('svg/sprite.svg#ok') }}"></use>
+                                    </svg>
+                                </a>
+                                <a href="">
+                                    <svg>
+                                        <use xlink:href="{{ asset('svg/sprite.svg#whatsapp') }}"></use>
+                                    </svg>
+                                </a>
+                                <a href="">
+                                    <svg>
+                                        <use xlink:href="{{ asset('svg/sprite.svg#djen') }}"></use>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/includes/modals.blade.php
+++ resources/views/includes/modals.blade.php
@@ -1,2 +1,1 @@
 <div id="shadow"></div>
-модалки
