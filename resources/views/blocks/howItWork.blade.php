448186a4aafef663cedc4c84a1e1028a90da7c71
artemyr <artem.affetta@gmail.com>
2024-01-13 16:58:50 +0300

    css

--- resources/views/blocks/howItWork.blade.php
+++ resources/views/blocks/howItWork.blade.php
@@ -1,18 +1,48 @@
 <div class="container">
-    <div class="article-2">
-        <div>
-            <div class="title-1">
-                Как это <span>работает?</span>
+    <div class="howItWork">
+        <div class="howItWork__wrapper">
+            <div>
+                <div class="article-2">
+                    <div>
+                        <div class="title-1">
+                            Как это <span>работает?</span>
+                        </div>
+                    </div>
+                    <div class="text">
+                        Мы тщательно работаем над каждым делом<br> и привлекаем самых опытных юристов
+                    </div>
+                    <a href="" class="ui-btn ui-btn-arrow">
+                        <span>Попробовать</span>
+                        <svg>
+                            <use xlink:href="{{ asset('svg/sprite.svg#arrow-1') }}"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <div class="howItWork__itemNumber">[ 1 ]</div>
+                        <div class="howItWork__itemTitle">Подбор юриста</div>
+                        <div class="howItWork__itemText">Наш опытный менеджер классифицирует ваш вопрос и подбирает юриста с опытом<br> успешных дел, похожих на ваше</div>
+                    </li>
+                    <li>
+                        <div class="howItWork__itemNumber">[ 2 ]</div>
+                        <div class="howItWork__itemTitle">Подготовка ответа</div>
+                        <div class="howItWork__itemText">Квалифицированный юрист из вашего региона получает ваш вопрос и готовится дать подробный и качественный ответ</div>
+                    </li>
+                    <li>
+                        <div class="howItWork__itemNumber">[ 3 ]</div>
+                        <div class="howItWork__itemTitle">Бесплатная консультация</div>
+                        <div class="howItWork__itemText">Юрист связывается с вами для бесплатной консультации и рассказывает как добиться нужного вам результата</div>
+                    </li>
+                    <li>
+                        <div class="howItWork__itemNumber">[ 4 ]</div>
+                        <div class="howItWork__itemTitle">Решение проблемы</div>
+                        <div class="howItWork__itemText">Теперь вы можете сделать всё самостоятельно или заказать услуги этого юриста, чтобы он сделал всю работу</div>
+                    </li>
+                </ul>
             </div>
         </div>
-        <div class="text">
-            Мы тщательно работаем над каждым делом<br> и привлекаем самых опытных юристов
-        </div>
-        <a href="" class="ui-btn ui-btn-arrow">
-            <span>Попробовать</span>
-            <svg>
-                <use xlink:href="{{ asset('svg/sprite.svg#arrow-1') }}"></use>
-            </svg>
-        </a>
     </div>
 </div>
