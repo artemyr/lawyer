f6234efe03b48bb06410f3eeea0cd76d5b2c76d6
artemyr <artem.r@affetta.ru>
2024-01-29 18:32:40 +0300

    new block

--- resources/views/blocks/ourJurists.blade.php
+++ resources/views/blocks/ourJurists.blade.php
@@ -7,4 +7,83 @@
         </div>
         <div></div>
     </div>
+
+    <div class="our-jurists">
+        <div class="our-jurists__wrapper">
+
+            <div class="our-jurists__item our-jurists-item">
+                <div class="our-jurists-item__image">
+                    <img src="{{ asset('image/jurists/1.jpg') }}" alt="">
+                </div>
+
+                <div class="our-jurists-item__data">
+                    <div class="our-jurists-item__name">
+                        Наталья Антонова
+                    </div>
+                    <div class="our-jurists-item__subtitle">
+                        Юрист · Стаж 13 лет
+                    </div>
+                    <ul class="our-jurists-item__list">
+                        <li>Антимонопольное и конкурентное право</li>
+                        <li>Комплаенс и внутренние расследования</li>
+                        <li>Фармацевтика и здравоохранение</li>
+                        <li>Производство, промышленность, транспорт</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="our-jurists__item our-jurists-item">
+                <div class="our-jurists-item__image">
+                    <img src="{{ asset('image/jurists/2.jpg') }}" alt="">
+                </div>
+
+                <div class="our-jurists-item__data">
+                    <div class="our-jurists-item__name">
+                        Владимир Белков
+                    </div>
+                    <div class="our-jurists-item__subtitle">
+                        Юрист · Стаж 13 лет
+                    </div>
+                    <ul class="our-jurists-item__list">
+                        <li>Недвижимость и строительство</li>
+                        <li>Потребительские товары и розничная торговля</li>
+                        <li>Производство, промышленность, транспорт</li>
+                        <li>Технологии, Медиа, Телекоммуникации</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="our-jurists__item our-jurists-item">
+                <div class="our-jurists-item__image">
+                    <img src="{{ asset('image/jurists/3.jpg') }}" alt="">
+                </div>
+
+                <div class="our-jurists-item__data">
+                    <div class="our-jurists-item__name">
+                        Дмитрий Артюшенко
+                    </div>
+                    <div class="our-jurists-item__subtitle">
+                        Юрист · Стаж 13 лет
+                    </div>
+                    <ul class="our-jurists-item__list">
+                        <li>Слияния и поглощения/Корпоративное право</li>
+                        <li>Производство, промышленность, транспорт</li>
+                        <li>Энергетика, горнодобывающая промышленность, инфраструктура</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="our-jurists__item our-jurists-mock-item">
+                <div class="our-jurists-mock-item__text">
+                    Опытные юристы и адвокаты готовы вам помочь
+                </div>
+                <a href="" class="ui-btn ui-btn-arrow ui-btn-arrow_white">
+                    <span>Получить консультацию</span>
+                    <svg>
+                        <use xlink:href="<?php echo e(asset('svg/sprite.svg#arrow-5')); ?>"></use>
+                    </svg>
+                </a>
+            </div>
+
+        </div>
+    </div>
+
 </div>
