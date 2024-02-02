1355ca6a24e26a07337ac33e0b92122e9a7926ae
artemyr <artem.r@affetta.ru>
2024-02-02 19:03:16 +0300

    new block

--- resources/views/blocks/recommends.blade.php
+++ resources/views/blocks/recommends.blade.php
@@ -9,4 +9,88 @@
             Уже более 200 000 человек получили <br>юридическую консультацию <br>на «Юристы КСТ»
         </div>
     </div>
+
+    <div class="recommends">
+        <div class="recommends__wrapper">
+
+            <div class="recommends-item">
+                <div>
+                    <div class="recommends-item__avatar">
+                        <img src="{{ asset('image/recommends/1.jpg') }}" alt="">
+                    </div>
+                    <div class="recommends-item__name">
+                        Еремова Анастасия
+                    </div>
+                    <div class="recommends-item__text">
+                        Отдельное спасибо хочется сказать откликнувшимся юристам за скорость и помощь в решении
+                        вопросов. Этот случай убедил меня ещё раз, что лично мне необходим этот сайт под рукой.
+                    </div>
+                </div>
+                <div>
+                    <div class="recommends-item__date">
+                        12.08.2023
+                    </div>
+                </div>
+            </div>
+            <div class="recommends-item">
+                <div>
+                    <div class="recommends-item__avatar">
+                        <img src="{{ asset('image/recommends/2.jpg') }}" alt="">
+                    </div>
+                    <div class="recommends-item__name">
+                        Игорь Махмудов
+                    </div>
+                    <div class="recommends-item__text">
+                        Скорый ответ позволил работать и спать спокойно. Премного благодарен) не припомню случая когда
+                        бесплатная помощь была оказана столь качественно. Спасибо!
+                    </div>
+                </div>
+                <div>
+                    <div class="recommends-item__date">
+                        12.08.2023
+                    </div>
+                </div>
+            </div>
+            <div class="recommends-item">
+                <div>
+                    <div class="recommends-item__avatar">
+                        <img src="{{ asset('image/recommends/3.jpg') }}" alt="">
+                    </div>
+                    <div class="recommends-item__name">
+                        Ксения Лаврентьева
+                    </div>
+                    <div class="recommends-item__text">
+                        Cрочно понадобилась юридическая помощь. Воспользовалась сервисом Все-Юристы.Ру, помогли решить
+                        проблему — просто выручили! Буду обращаться ещё. Также буду очень рада вновь обратится к вам
+                    </div>
+                </div>
+                <div>
+                    <div class="recommends-item__date">
+                        12.08.2023
+                    </div>
+                </div>
+            </div>
+            <div class="recommends-item">
+                <div>
+                    <div class="recommends-item__avatar">
+                        <img src="{{ asset('image/recommends/4.jpg') }}" alt="">
+                    </div>
+                    <div class="recommends-item__name">
+                        Максим Машкин
+                    </div>
+                    <div class="recommends-item__text">
+                        Отдельное спасибо хочется сказать откликнувшимся юристам за скорость и помощь в решении
+                        вопросов.
+                        Этот случай убедил меня ещё раз, что лично мне необходим этот сайт под рукой.
+                    </div>
+                </div>
+                <div>
+                    <div class="recommends-item__date">
+                        12.08.2023
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </div>
